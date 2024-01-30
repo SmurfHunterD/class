47e18a614eaf8c3173927159cc88aa264899c458
SmurfHunterD <SmurfHunterD@proton.me>
2024-01-29 20:57:12 -0500

    update to classwork

--- cpp/project3/main.cpp
+++ cpp/project3/main.cpp
@@ -13,8 +13,7 @@ using namespace std;
 int main(){
 
     // Initialize Variables
-    int numberOfPayments;
-    double loanAmount, amountPaidBack, yearlyRate, rate; 
+    double numberOfPayments, loanAmount, yearlyRate; 
 
     // Collect User Input for Calculations
     cout << "What is your total loan amount? " << endl;
@@ -25,20 +24,21 @@ int main(){
     cin >> numberOfPayments;
 
     // calculate monthly interest rate for monthly payment calculation
-    double monthlyRate = (yearlyRate/100) / 12.0;
+    double monthlyRate = (yearlyRate / 100) / 12.0;
     double monthlyPayment = (monthlyRate * pow(1 + monthlyRate, numberOfPayments)) / (pow(1+ monthlyRate, numberOfPayments) - 1) * loanAmount;
-    double interestPaid = loanAmount / yearlyRate;
+    double amountPaid = ((monthlyPayment * monthlyRate) * numberOfPayments) * 100;
+    double interestPaid = amountPaid - loanAmount;
 
     // Set Currency Values to 2 decimals
     cout << setprecision(2) << right << fixed;
 
     // Calculations and output
-    cout <<  "Loan Amount: " << setw(15) << "$ " << loanAmount << endl;
-    cout << "Monthly Interest Rate : " << setw(11) << static_cast<int>(monthlyRate) << "%" << endl;
-    cout << "Number of Payments: " << setw(16) << numberOfPayments << endl;
-    cout << "Monthly Payment: " << setw(13) << "$ " << monthlyPayment << endl;
-    cout << "Amount Paid Back: " << setw(10) << "$ " << loanAmount + interestPaid << endl;
-    cout << "Interest Paid: " << setw(15) << "$ " << interestPaid << endl;
+    cout << "Loan Amount: " << setw(15) << "$ " << loanAmount << endl;
+    cout << "Monthly Interest Rate : " << setw(11) << static_cast<int>(monthlyRate * 100) << "%" << endl;
+    cout << "Number of Payments: " << setw(16) << static_cast<int>(numberOfPayments) << endl;
+    cout << "Monthly Payment: " << setw(13) << "$   " << monthlyPayment << endl;
+    cout << "Amount Paid Back: " << setw(10) << "$ " << amountPaid << endl;
+    cout << "Interest Paid: " << setw(14) << "$  " << interestPaid << endl;
 
     return 0;
 }

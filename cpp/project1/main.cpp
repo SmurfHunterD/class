205c74f0e5198b31f16356d3dccae75b8d8906a0
SmurfHunterD <91677817+SmurfHunterD@users.noreply.github.com>
2024-03-17 17:07:36 -0400

    Add files via upload

---
+++ cpp/project1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Angelo Edward", favFood = "sushi", favColor = "green";
+    
+    cout << "Hello, and welcome to my program for practice1.\n" "My name is " << name << + ",\n";
+    cout << "my favorite food is " << favFood << + ",\n"  "and my favorite color is " << favColor << + ".\n";
+}
